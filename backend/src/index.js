751372ecae9053204456afa5d226a88f604ffe54
tiagosousa <tiagosousa.tams@hotmail.com>
2025-07-21 20:51:21 +0100

    create auth routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import "dotenv/config";
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

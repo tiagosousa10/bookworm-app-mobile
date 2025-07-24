ac3b3d63cb990214ecdf9891c8473a6772c553dd
tiagosousa <tiagosousa.tams@hotmail.com>
2025-07-24 17:30:06 +0100

    create a new book, and store it in cloudinary and db

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -1,10 +1,35 @@
 import express from "express";
+import Book from "../models/Book.js";
+import cloudinary from "../lib/cloudinary";
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
+router.post("/", protectRoute, async (req, res) => {
   try {
-  } catch (error) {}
+    const { title, caption, image, rating } = req.body;
+    if (!title || !caption || !image || !rating) {
+      return res.status(400).json({ error: "Missing fields" });
+    }
+
+    //upload image to cloudinary
+    const uploadResponse = await cloudinary.uploader.upload(image);
+    const imageUrl = uploadResponse.secure_url;
+    //save to databse
+    const newBook = new Book({
+      title,
+      caption,
+      image: imageUrl,
+      rating,
+      // user: req.user._id,
+    });
+
+    await newBook.save();
+
+    res.status(201).json(newBook);
+  } catch (error) {
+    console.log("Error creating book:", error);
+    res.status(500).json({ error: "Error creating book" });
+  }
 });
 
 export default router;

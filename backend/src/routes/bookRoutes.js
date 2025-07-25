168eb29cb2607c778b26e96beebe054767acc913
tiagosousa <tiagosousa.tams@hotmail.com>
2025-07-25 17:24:24 +0100

    delete a book

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -59,4 +59,34 @@ router.get("/", protectRoute, async (req, res) => {
   }
 });
 
+router.delete("/:id", protectRoute, async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) return res.status(404).json({ error: "Book not found" });
+
+    //check if user is the creator of the book
+    if (book.user.toString() !== req.user._id.toString()) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    //delete image from cloudinary
+    if (book.image && book.image.includes("cloudinary")) {
+      try {
+        const publicId = book.image.split("/").pop().split(".")[0];
+        await cloudinary.uploader.destroy(publicId);
+      } catch (error) {
+        console.log("Error deleting image from cloudinary:", error);
+      }
+    }
+
+    //delete from DB
+    await book.deleteOne();
+
+    res.status(200).json({ message: "Book deleted successfully" });
+  } catch (error) {
+    console.log("Error deleting book:", error);
+    res.status(500).json({ error: "Error deleting book" });
+  }
+});
+
 export default router;

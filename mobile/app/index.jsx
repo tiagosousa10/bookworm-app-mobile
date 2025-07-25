e0a56688300bbb5b38348b77575932406630c08b
tiagosousa <tiagosousa.tams@hotmail.com>
2025-07-25 17:40:19 +0100

    set up frontend environment

---
+++ mobile/app/index.jsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from "react-native";
+import { Image } from "expo-image";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>123</Text>
+      <Image />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    color: "blue",
+  },
+});

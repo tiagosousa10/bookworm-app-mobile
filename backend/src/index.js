db88d82308e4606cf964925bbc238485f7ec9a72
tiagosousa <tiagosousa.tams@hotmail.com>
2025-07-27 23:04:56 +0100

    create authentication routes and safescreen style

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,13 @@ import "dotenv/config";
 import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); // Parse JSON request bodies
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);

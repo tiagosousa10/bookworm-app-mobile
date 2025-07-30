c9178ef5b245d03be937f1cb5ee9e4dbce01167c
tiagosousa <tiagosousa.tams@hotmail.com>
2025-07-30 18:36:15 +0100

    configure tabs menu and layout to verifiy user and token

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,9 +1,32 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter, useSegments } from "expo-router";
 import SafeScreen from "../components/SafeScreen";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { useAuthStore } from "../store/authStore";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const router = useRouter();
+  const segments = useSegments();
+
+  const { checkAuth, user, token } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  //handle navigation based on the auth state
+  useEffect(() => {
+    const inAuthScreen = segments[0] === "(auth)";
+    const isSignedIn = user && token;
+
+    if (!isSignedIn && !inAuthScreen) {
+      router.replace("/(auth)");
+    } else if (isSignedIn && inAuthScreen) {
+      router.replace("/(tabs)");
+    }
+  }, [user, token, segments]);
+
   return (
     <SafeAreaProvider>
       <SafeScreen>

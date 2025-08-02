bc48b3d5b4734d051e4bdb40e07bb5e21f1223fb
tiagosousa <tiagosousa.tams@hotmail.com>
2025-08-02 18:03:28 +0100

    first jsx form the home page with a flatlist

--- mobile/app/(tabs)/index.jsx
+++ mobile/app/(tabs)/index.jsx
@@ -1,16 +1,48 @@
-import { View, Text, TouchableOpacity } from "react-native";
-import React from "react";
+import { View, Text, TouchableOpacity, FlatList, Image } from "react-native";
+import React, { useEffect, useState } from "react";
 import { useAuthStore } from "../../store/authStore";
 
+import styles from "../../assets/styles/home.styles";
+
 const Home = () => {
-  const { logout } = useAuthStore();
+  const { token } = useAuthStore();
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+
+  const fetchBooks = async (pageNum = 1, refresh = false) => {};
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const handleLoadMore = async () => {};
+
+  const renderItem = ({ item }) => (
+    <View style={styles.bookCard}>
+      <View style={styles.bookHeader}>
+        <View style={styles.userInfo}>
+          <Image
+            source={{ uri: item.user.profileImage }}
+            style={styles.avatar}
+          />
+          <Text>{item.user.username}</Text>
+        </View>
+      </View>
+    </View>
+  );
 
   return (
-    <View>
-      <Text>Home</Text>
-      <TouchableOpacity onPress={logout}>
-        <Text>logout</Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+      <FlatList
+        data={books}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={styles.listContainer}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   );
 };

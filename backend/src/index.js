6089f3fc00ecf31fc132f5660cf23e47632dcf04
tiagosousa <tiagosousa.tams@hotmail.com>
2025-08-02 17:56:56 +0100

    update api url

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,7 @@ import job from "./lib/cron.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// job.start();
+job.start();
 
 app.use(express.json({ limit: "10mb" })); // Parse JSON request bodies
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -1,2 +1,3 @@
-// export const API_URL = "https://bookworm-app-mobile.onrender.com/api"; -> RENDER -> tem limite de 1MB -> Incompativel com imagens
-export const API_URL = "https://c8635f2395d0.ngrok-free.app/api"; // NGROK
+export const API_URL = "https://bookworm-app-mobile.onrender.com/api";
+//  -> RENDER -> tem limite de 1MB -> Incompativel com imagens
+// export const API_URL = "https://fef9b669a9e0.ngrok-free.app/api"; // NGROK
